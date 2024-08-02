a2f991b0667e02e0d5c6066a59726c5371a1e0cd
MadhuByreddi116 <madhubyreddi116@gmail.com>
2024-08-02 12:08:43 +0530

    initial commit

---
+++ REACT_2_ASSIGNMENT_6/src/ToDo.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './todo.css';
+
+const ToDoWebApp = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { text: newTask, completed: false }]);
+      setNewTask('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+  const toggleTaskCompletion = (index) => {
+    const newTasks = tasks.map((task, i) => {
+      if (i === index) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="todotask">
+      <h1>SIMPLE REACT TO-DO WEB APP </h1>
+      <div className="task-input">
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Enter & Add a New Task"
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul className="task-list">
+        {tasks.map((task, index) => (
+          <li key={index} className={task.completed ? 'completed' : ''}>
+            <span onClick={() => toggleTaskCompletion(index)}>{task.text}</span>
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDoWebApp;
---
+++ REACT_2_ASSIGNMENT_6/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+import Calculator from './Calculator';
+import ToDOFun from './ToDo';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <ToDOFun />  
+  
+);
+
